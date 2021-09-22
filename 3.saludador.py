1103e3387f7cfc96a1e85f2dea5d8332b66711fd
luis14423 <90762242+luis14423@users.noreply.github.com>
2021-09-22 11:58:23 +0200

    Add files via upload

---
+++ 3.saludador.py
@@ -0,0 +1,4 @@
+def saludador():
+    nombre= raw_input("Como te llamas? ")
+    print("Buenos dias, "+nombre)
+saludador()
---
+++ 7.tabla_1.py
@@ -0,0 +1,10 @@
+def tabla_1():
+    print("***********************")
+    print("*    TABLA DEL 7      *")
+    print("***********************")
+    for cont in range(0,11):
+        print("7 x "+str(cont)+" = "+str(7*cont))
+    print("FIN")
+    
+tabla_1()
+
