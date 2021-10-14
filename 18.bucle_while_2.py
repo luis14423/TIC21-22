c9526d4d5fbc46fdec8a8ee33235addec7265382
luis14423 <90762242+luis14423@users.noreply.github.com>
2021-10-14 07:52:14 +0100

    Add files via upload

---
+++ 18.bucle_while_2.py
@@ -0,0 +1,8 @@
+def bucle_while_2():
+    cont=1
+    while(cont<100):
+        print("numero "+str(cont))
+        cont=cont+1
+bucle_while_2()
+        
+    
---
+++ 20.tabla_while.py
@@ -0,0 +1,13 @@
+def tabla():
+    x=input("Dime un numero: ")
+    cont=0
+    while (cont<11 and continuar=="S"):
+        print(str(x)+" por "+str(cont)+ " = "+str(x*cont))
+        cont=cont+1
+    
+tabla()
+
+            
+            
+    
+    
