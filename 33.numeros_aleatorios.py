15fa2eac054faecbd3871c7ddad393784610450e
luis14423 <90762242+luis14423@users.noreply.github.com>
2021-11-17 12:01:06 +0100

    Add files via upload

---
+++ 33.numeros_aleatorios.py
@@ -0,0 +1,11 @@
+import random
+def suerte():
+
+    for cont in range(1,11):
+        num=random.randint(1,10)
+        print(num)
+suerte()
+
+#deberes:
+"""UN PROGRAMA DE DADOS: 3 TIRADAS. Gana el que sume el numero mayor"""
+
