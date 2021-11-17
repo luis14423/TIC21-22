a61af953e32b8c0c506c8a2b1abaca3efb7be7fc
luis14423 <90762242+luis14423@users.noreply.github.com>
2021-11-17 11:52:27 +0100

    Add files via upload

---
+++ 32.programa_a_partit_de_flujo.py
@@ -0,0 +1,17 @@
+def f():
+    num=input("Dame un numero: ")
+    suma=1
+    for cont in range(num,-1,-1):
+        print(cont,suma)
+        suma=suma*cont
+
+        
+    print(cont,suma)
+
+
+
+
+f()
+
+
+    
